14f67bf53b879dcfdbf2c5ce5ffc06f02af28eeb
Ledka17 <ledkaconf@gmail.com>
2019-02-08 23:06:35 +0300

    added ex1

---
+++ ex1.py
@@ -0,0 +1,16 @@
+s1 = 'разработка'
+s2 = 'сокет'
+s3 = 'декоратор'
+
+print(s1, type(s1))
+print(s2, type(s2))
+print(s3, type(s3))
+print()
+
+s1_b = s1.encode('utf-8')
+s2_b = s2.encode('utf-8')
+s3_b = s3.encode('utf-8')
+
+print(s1_b, type(s1_b))
+print(s2_b, type(s2_b))
+print(s3_b, type(s3_b))
