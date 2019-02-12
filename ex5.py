8a538a855c9de47faceb0ec7624defd47db216ad
Ledka17 <30876075+Ledka17@users.noreply.github.com>
2019-02-12 21:20:24 +0300

    Delete ex5.py

--- ex5.py
+++
@@ -1,9 +0,0 @@
-import subprocess as sp
-
-args = ['yandex.ru', 'youtube.com']
-
-s_ping = sp.Popen(args, stdout=sp.PIPE)
-
-for line in s_ping.stdout:
-  line = line.decode('cp866').encode('utf-8')
-  print(line.decode('utf-8'))
