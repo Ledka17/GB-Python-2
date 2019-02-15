0609427a4d9486dc93aa3a6f78a8406f81dddf60
Ledka17 <ledkaconf@gmail.com>
2019-02-16 02:03:09 +0300

    changed client prog

--- client.py
+++ client.py
@@ -1,10 +1,19 @@
 # Программа клиента
 
 from socket import *
+import time
 
 s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
 s.connect(('localhost', 7777))   # Соединиться с сервером
-msg = 'Привет, сервер! Как дела?' #сформировать presence-сообщение
+presence_msg = {
+        "action": "presence",
+        "time": int(time.time()),
+        "type": "status",
+        "user": {
+                "account_name":  "Leonarda",
+                "status":      "I'm here!"
+        }
+} #сформировать presence-сообщение
 s.send(msg.encode('utf-8')) #отправить сообщение серверу
 data = s.recv(1000000) #получить ответ сервера
 print('Сообщение от сервера: ', data.decode('utf-8')) #разобрать сообщение сервера
